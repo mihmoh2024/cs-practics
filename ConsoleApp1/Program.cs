057b82c6ee203046e0e237e951e2384f87bea188
mihmohpihpoh07@gmail.com <mihmohpihpoh07@gmail.com>
2021-10-11 11:12:22 +0300

    practic 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,35 +1,21 @@
 ﻿using System;
 
-namespace pr1
+namespace _1
 {
-  struct Person
-  {
-    public string firstname;
-    public string lastname;
-    public int age;
-    public enum Gender : int { Male, Female };
-    public Gender gender;
-    public Person(string _firstname, string _lastname, int _age, Gender _gender)
-    {
-      firstname = _firstname;
-      lastname = _lastname;
-      age = _age;
-      gender = _gender;
-  }
-    public override string ToString()
-    {
-      return firstname + " " + lastname + ",age" + age + " " + gender;
-    }
-  }
-
   class Program
   {
     static void Main(string[] args)
-    {
-      Person p = new Person("Mihail", "Smirnov", 19, Person.Gender.Male);
-      Console.WriteLine(p);
-      Console.ReadKey();
-    }
+    { 
+      string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+      string[] sa = s.Split(' ');
+      Array.Sort(sa);
+      s = string.Join(" ", sa);
+      Console.WriteLine(s);
+      Console.ReadLine();
 
+    }
   }
 }
+
+
+  
